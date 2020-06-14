7ca6158adfdba3d952639b6325b0fb1692836e0c
iamNamanKhare <namankhare42@gmail.com>
2020-06-14 18:06:15 +0530

    Add Environment Variables

--- routes/api/news-route.js
+++ routes/api/news-route.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const axios = require('axios').default
-const keys = require('../../config/keys')
+require('dotenv').config()
 
 router.get('/', (req, res) => {
     console.log('[ GET REQUEST ] /api/news')
     
-    axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY||keys.news.API_KEY}`)
+    axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}`)
         .then((response) => {
             res.json(response.data)
         })
--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const notesRoute = require('./routes/api/notes-routes')
 const newsRoute = require('./routes/api/news-route')
 const passport = require('./config/passport-setup')
 const mongoose = require('mongoose')
-const keys = require('./config/keys')
 const cookieSession = require('cookie-session')
 const cors = require('cors')
+require('dotenv').config()
 
 const port = process.env.PORT || 4000 
 
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(cookieSession({
     maxAge: 24*60*60*1000,
-    keys: [process.env.cookieKey||keys.session.cookieKey]
+    keys: [process.env.cookieKey]
 }))
 
 app.use(passport.initialize())
@@ -27,7 +27,7 @@ app.use(passport.session())
 
 app.use(cors())
 
-mongoose.connect(process.env.dbURI||keys.mongodb.dbURI,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
+mongoose.connect(process.env.dbURI,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log('Connected to Database')
 })
 
